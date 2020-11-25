1a65d57de05fe9aa25bccf443e295641e33aee48
arkady likovizki <arkados2@gmail.com>
2020-11-25 15:23:17 +0200

    calender comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Daybook from "./Daybook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Rooms Management
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Daybook />
     </div>
   );
 }
---
+++ src/Daybook.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { TextField, Container } from "@material-ui/core";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+  DatePicker,
+} from "@material-ui/pickers";
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
+import { he } from "date-fns/locale";
+
+import useStyles from "./styles";
+
+const Daybook = () => {
+  const classes = useStyles();
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleDateChange = (date) => {
+    console.log(date);
+    setSelectedDate(date);
+  };
+  return (
+    <>
+      <h1>date picker</h1>
+
+      <Container>
+        <MuiPickersUtilsProvider locale={he} utils={DateFnsUtils}>
+          <DatePicker
+            variant='static'
+            value={selectedDate}
+            onChange={handleDateChange}
+            orientation='landscape'
+          />
+        </MuiPickersUtilsProvider>
+      </Container>
+    </>
+  );
+};
+
+export default Daybook;
