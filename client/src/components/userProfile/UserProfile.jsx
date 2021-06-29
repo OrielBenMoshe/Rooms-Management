8ce8563c7d1fa69d1142606d0c9cfb6414226178
OrielBenMoshe <ori.l.2m@gmail.com>
2021-06-30 00:08:00 +0300

    UserProfile styling - user details

--- client/src/components/userProfile/UserProfile.jsx
+++ client/src/components/userProfile/UserProfile.jsx
@@ -22,7 +22,7 @@ import { styles } from "@material-ui/pickers/views/Calendar/Calendar";
 //togggle between two pages
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
-  
+
   return (
     <div
       role="tabpanel"
@@ -66,108 +66,63 @@ function UserProfile({ history }) {
   };
 
   return (
-      <div id="UserProfile">
-        <Header/>
-            <Card className="greeting" variant="none" elevation={0}>
-              <Button
-                onClick={() => {
-                  history.goBack();
-                }}
-                style={{ position: "relative", right: "145px", top: "-60px" }}
-              >
-                <ArrowBack style={{ color: "#00aaaf" }} />
-              </Button>
-              <div>
-                <AccountCircleOutlinedIcon
-                  style={{ fontSize: "70px" }}
-                  fontSize="large"
-                />
-                <CameraAltRounded
-                  style={{
-                    color: "#00AAAF",
-                    position: "relative",
-                    left: "22px",
-                  }}
-                  fontSize="small"
-                />
-              </div>
-
-              <Typography
-                color="textPrimary"
-                gutterBottom
-                variant="body2"
-                component="p"
-                className=""
-              >
-                {user.email}
-                <EditOutlined
-                  fontSize="small"
-                  style={{
-                    color: "#00AAAF",
-                    marginTop: "10px",
-                    position: "relative",
-                    top: "4px",
-                  }}
-                  variant="outline"
-                />
-              </Typography>
-              <Typography
-                color="textPrimary"
-                gutterBottom
-                variant="body2"
-                component="p"
-                className=""
-              >
-                {user.phone}
-                <EditOutlined
-                  fontSize="small"
-                  style={{
-                    color: "#00AAAF",
-                    marginTop: "10px",
-                    position: "relative",
-                    top: "4px",
-                  }}
-                  variant="outline"
-                />
-              </Typography>
-            </Card>
-          
-          <AppBar variant="none" position="static" color="default">
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              indicatorColor="primary"
-              textColor="primary"
-              variant="fullWidth"
-              aria-label="full width tabs example"
-            >
-              <Tab
-                className="tab"
-                label="חדרים מוזמנים"
-                {...a11yProps(0)}
-              />
-              <Tab
-                className="tab"
-                label="הסטורית הזמנות"
-                {...a11yProps(1)}
-              />
-            </Tabs>
-          </AppBar>
-
-          <SwipeableViews
-            axis="x-reverse"
-            index={value}
-            onChangeIndex={handleChangeIndex}
-          >
-            <TabPanel value={value} index={0}>
-              <Item />
-              <Item />
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              <Item />
-            </TabPanel>
-          </SwipeableViews>
+    <div className="userProfile">
+      <Header />
+      <Card className="user_details_container" variant="none" elevation={0}>
+        <Button onClick={ () => { history.goBack();} } >
+          <ArrowBack/>
+        </Button>
+        <div className="user_image">
+          <img className="avatar" src="/images/icon_avatar.svg" alt="" />
+          <img className="camera" src="/images/icon_camera.svg" alt="" />
         </div>
+
+        <Typography>
+          <img className="edit" src="/images/icon_pencil.svg" alt="" />
+          {user.email}
+        </Typography>
+        <Typography>
+          <img className="edit" src="/images/icon_pencil.svg" alt="" /> 
+          {user.phone}
+        </Typography>
+      </Card>
+
+      <AppBar variant="none" position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab
+            className="tab"
+            label="חדרים מוזמנים"
+            {...a11yProps(0)}
+          />
+          <Tab
+            className="tab"
+            label="הסטורית הזמנות"
+            {...a11yProps(1)}
+          />
+        </Tabs>
+      </AppBar>
+
+      <SwipeableViews
+        axis="x-reverse"
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0}>
+          <Item />
+          <Item />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Item />
+        </TabPanel>
+      </SwipeableViews>
+    </div>
   );
 }
 export default withRouter(UserProfile);
